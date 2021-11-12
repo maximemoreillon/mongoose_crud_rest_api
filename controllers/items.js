7b7c03e4ae01359bdac1296f360201413f060895
Maxime Moreillon <m.moreillon@gmail.com>
2021-11-12 15:14:56 +0900

    U

--- controllers/items.js
+++ controllers/items.js
@@ -2,32 +2,60 @@ const Item = require('../models/item.js')
 const { error_handling } = require('../utils.js')
 
 exports.read_items = async (req, res) => {
-  const query = {}
-  const items = await Item.find(query)
-  res.send(items)
-  console.log(`[Mongoose] Items queried`)
+  try {
+    const query = {}
+    const items = await Item.find(query)
+    res.send(items)
+    console.log(`[Mongoose] Items queried`)
+  } catch (error) {
+    error_handling(error,res)
+  }
+
 }
 
 exports.create_item = async (req, res) => {
-  const properties = req.body
-  const item = await Item.create(properties)
-  res.send(item)
-  console.log(`[Mongoose] Item ${item._id} created`)
+  try {
+    const properties = req.body
+    const item = await Item.create(properties)
+    res.send(item)
+    console.log(`[Mongoose] Item ${item._id} created`)
+  } catch (error) {
+    error_handling(error,res)
+  }
+
 
 }
 
 exports.read_item = async (req, res) => {
-  const {_id} = req.params
-  const item = await Item.findOne({_id})
-  res.send(item)
+  try {
+    const {_id} = req.params
+    const item = await Item.findOne({_id})
+    res.send(item)
+  } catch (error) {
+    error_handling(error,res)
+  }
+
 }
 
 exports.update_item = async (req, res) => {
-  res.status(501).send('Not implemented')
+  try {
+    const {_id} = req.params
+    const properties = req.body
+    const result = await Item.findOneAndUpdate({_id},properties)
+    res.send(result)
+  } catch (error) {
+    error_handling(error,res)
+  }
+
 }
 
 exports.delete_item = async (req, res) => {
-  const {_id} = req.params
-  const result = await Item.findOneAndDelete({_id})
-  res.send(result)
+  try {
+    const {_id} = req.params
+    const result = await Item.findOneAndDelete({_id})
+    res.send(result)
+  } catch (error) {
+    error_handling(error,res)
+  }
+
 }
