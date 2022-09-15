cdbf9c5e392b7aad825e50241b14fe4eae97583b
Maxime Moreillon <m.moreillon@gmail.com>
2022-09-15 14:29:57 +0900

    minor

--- models/movie.js
+++ models/movie.js
@@ -2,10 +2,7 @@ const {Schema, model, Types} = require('mongoose')
 
 const schema = new Schema({
   title: String,
-  director: {
-    type: Types.ObjectId,
-    ref: 'Person'
-  },
+  director: { type: Types.ObjectId, ref: 'Person' },
 })
 
 const Movie = model('Movie', schema)
