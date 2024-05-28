814de89e4c665d9e5cd3a7c78eb9b03b44b7866e
Maxime Moreillon <m.moreillon@gmail.com>
2024-05-28 22:13:15 +0900

    MongoDB connection env

--- db.ts
+++ db.ts
@@ -1,14 +1,34 @@
 import mongoose from "mongoose"
 
-const { MONGODB_URL = "mongodb://localhost", MONGODB_DB = "mevn_crud" } =
-  process.env
+export const {
+  MONGODB_CONNECTION_STRING,
+  MONGODB_PROTOCOL = "mongodb",
+  MONGODB_USERNAME,
+  MONGODB_PASSWORD,
+  MONGODB_HOST = "mongo",
+  MONGODB_PORT,
+  MONGODB_DB = "example",
+  MONGODB_OPTIONS = "",
+} = process.env
+
+const mongodbPort = MONGODB_PORT ? `:${MONGODB_PORT}` : ""
+
+const connectionString =
+  MONGODB_CONNECTION_STRING ||
+  (MONGODB_USERNAME && MONGODB_PASSWORD
+    ? `${MONGODB_PROTOCOL}://${MONGODB_USERNAME}:${MONGODB_PASSWORD}@${MONGODB_HOST}${mongodbPort}/${MONGODB_DB}${MONGODB_OPTIONS}`
+    : `${MONGODB_PROTOCOL}://${MONGODB_HOST}${mongodbPort}/${MONGODB_DB}${MONGODB_OPTIONS}`)
+
+export const redactedConnectionString = connectionString.replace(
+  /:.*@/,
+  "://***:***@"
+)
 
 export const connect = async () => {
   try {
-    const connection_url = `${MONGODB_URL}/${MONGODB_DB}`
-    console.log(`[Mongoose] Connecting to ${connection_url}`)
+    console.log(`[Mongoose] Connecting to ${redactedConnectionString}`)
 
-    await mongoose.connect(connection_url)
+    await mongoose.connect(connectionString)
     console.log(`[Mongoose] MongoDB connected`)
   } catch (e) {
     console.log(`[Mongoose] MongoDB connection ERROR`)
