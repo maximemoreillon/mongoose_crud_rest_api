004a3faa2ce92b3baa2dc5ca60d3434f47c7fab3
Maxime Moreillon <m.moreillon@gmail.com>
2025-08-26 09:08:10 +0900

    redactedUrl

--- db.ts
+++ db.ts
@@ -3,14 +3,14 @@ import mongoose from "mongoose";
 export const { MONGODB_URI = "mongodb://localhost:27017/movies-crud" } =
   process.env;
 
-export const redactedConnectionString = MONGODB_URI.replace(
-  /:.*@/,
-  "://***:***@"
-);
+const redactedUrl = new URL(MONGODB_URI);
+if (redactedUrl.username) redactedUrl.username = "***";
+if (redactedUrl.password) redactedUrl.password = "***";
+export const redactedConnectionString = redactedUrl.href;
 
 export const connect = async () => {
   try {
-    console.log(`[Mongoose] Connecting to ${redactedConnectionString}`);
+    console.log(`[Mongoose] Connecting to DB ${redactedConnectionString}`);
 
     await mongoose.connect(MONGODB_URI);
     console.log(`[Mongoose] MongoDB connected`);
--- models/movie.ts
+++ models/movie.ts
@@ -1,4 +1,4 @@
-import { Schema, model, Types } from "mongoose";
+import { Schema, model } from "mongoose";
 
 interface IMovie {
   title: string;
